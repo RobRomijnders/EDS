92fb9cbe583e6fd8e0d960f83e68c09d3b542bd9
robromijnders <romijndersrob@gmail.com>
2016-11-08 15:30:40 +0100

    upload files and readme

---
+++ gen_sr.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 18:00:54 2016
+
+@author: rob
+"""
+import numpy as np
+def swiss_roll(ppm=2500, std=2, scale=10, width = 1.0):
+  centers = np.array([[0.75, 0.75], [0.75, 1.25],[1.25, 0.75],[ 1.25, 1.25]])
+  centers *= scale
+  M = centers.shape[0]
+  data = []
+  labels = []
+  for m in range(M):
+    data.append(width*np.random.randn(ppm,2)+centers[m,:])
+    labels.append(m*np.ones((ppm,1)))
+  data = np.concatenate(data,axis=0)
+  N = data.shape[0]
+  labels = np.concatenate(labels,axis=0)
+  data = np.vstack((data[:,0]*np.cos(data[:,0]), data[:,1], data[:,0]*np.sin(data[:,0]))).T
+  data += std*np.random.randn(N,3)
+
+  return data, labels
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Eindhoven Data Science
+This github repo supplies material for the Eindhoven Data Science MeetUp
+
+Next MeetUp is 17 Nov 2016 on Unsupervised learning
+
